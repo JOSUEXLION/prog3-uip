c60448aceb0cc8ce36a5fa8570b7f37363db286f
JOSUEXLION <DOMINID128@GMAIL.COM>
2015-09-21 22:02:10 -0500

    Create quiz.py

---
+++ laboratorios/quiz.py
@@ -0,0 +1,16 @@
+#josue de leon
+
+base = 5 
+altura = 7
+area = (base*altura)
+print ("El area en centimetros es "+ str(area)) #impresion del area en centimetro
+perimetro = 2*(altura+base)
+print ("el perimetro en centimetros es "+ str(perimetro)) #impresion del perimetro en centimetro
+area = (base*altura)/100
+print ("El area en metros es "+ str(area)) #impresion del area en metros
+perimetro = (2*(altura+base))/100 
+print ("el perimetro en metros es "+ str(perimetro)) #impresion del perimetro en metros
+area = (base*altura*0.39)
+print ("El area en pulgadas es "+ str(area)) #impresion del area en pulgadas
+perimetro = (2*(base+altura)*0.39) 
+print ("el perimetro en pulgadas es "+ str(perimetro)) #impresion del perimetro en pulgadas
