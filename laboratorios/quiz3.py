8bd3875a05be2033b7320280dffae239308a9421
JOSUEXLION <DOMINID128@GMAIL.COM>
2015-10-05 21:41:34 -0500

    Create quiz3.py

---
+++ laboratorios/quiz3.py
@@ -0,0 +1,19 @@
+""""dado el intervalo de tiempo en segundos, 
+calcular los segundos restantes 
+que corresponden para convertirse exactamente en minutos. 
+este programa debe funcionarar 5 oportunidades
+"""
+repeticiones =[]
+numRepeticiones = 5
+j=0
+while j < numRepeticiones:
+	j+=1
+	d=input("Ingrese numeros del" +str(j)+"intervalo:")
+	rep.append(float(d))
+	l=0
+for tiempo in Repeticiones:
+	l+=1
+	restantes=tiempo%60
+	minunts=60-restantes
+	print("los"+str(l)+ "los segundos son:"+srt(minunts))
+
